17961fdb807b16f2e157adb97cd018542264eff0
Hedris Pereira <hedris.pereira@eemovel.com>
2025-04-10 15:16:15 -0300

    fix

---
+++ src/bot/Ro.js
@@ -0,0 +1,12 @@
+import WebDriverManagerRo from "../core/WebDriverManagerRo";
+
+
+
+function startBot() {
+    // Inicializa o bot
+    const bot = new WebDriverManagerRo('chrome');
+    bot.start();
+}
+
+
+startBot();
